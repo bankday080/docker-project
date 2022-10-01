ce6f716f5d968dedd7859e51322bf08248518515
bankday080 <bankday02@gmail.com>
2022-10-01 10:12:02 +0700

    no message

--- backend/server.js
+++ backend/server.js
@@ -1,16 +1,11 @@
-var express = require('express');
-var app = express();
-var fs = require("fs");
+const express = require('express')
+const app = express()
 
-app.get('/listUsers', function (req, res) {
-   fs.readFile( __dirname + "/" + "users.json", 'utf8', function (err, data) {
-      console.log( data );
-      res.end( data );
-   });
+app.get('/',(req,res) => {
+   res.json({status: 200, data: "hello ...."})
+
 })
 
-var server = app.listen(8081, function () {
-   var host = server.address().address
-   var port = server.address().port
-   console.log("Example app listening at http://%s:%s", host, port)
+app.listen(3000, () => {
+   console,log('Start server at port 3000.')
 })
