967ff25ca62389a977bbf6c268d49d85a571e5b1
bankday080 <bankday02@gmail.com>
2022-10-15 18:43:30 +0700

    no message

--- firebase.js
+++ firebase.js
@@ -1,18 +1,22 @@
-// import {initializeApp} from "firebase/app";
-// import {getDatabase} from "firebase/database";
 
-// const firebaseConfig = {
-//         apiKey: "AIzaSyD0-Ezcn_aYI76yOM78FleXKsLDtivlUwI",
-//         authDomain: "test1-cbbc2.firebaseapp.com",
-//         databaseURL: "https://test1-cbbc2-default-rtdb.asia-southeast1.firebasedatabase.app",
-//         projectId: "test1-cbbc2",
-//         storageBucket: "test1-cbbc2.appspot.com",
-//         messagingSenderId: "862842099852",
-//         appId: "1:862842099852:web:5356c38f0a970b452dc3a9",
-//         measurementId: "G-R73HXYEW3J"
-    
-// };
+/*----------------------------------------part1----------------------------------------*/
+var admin = require("firebase-admin");
 
-// const app = initializeApp(firebaseConfig);
-// export const database = getDatabase(app);
+var serviceAccount = require("./serviceAccountKEY.json");
+const { query } = require("express");
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://egg-project-f7924-default-rtdb.asia-southeast1.firebasedatabase.app"
+});
+
+const db = admin.firestore();
+
+
+delete documents
+db.collection("Cutomer").doc("0").delete().then(res => {
+    console.log("document deleted succrsffuly")
+});
+
+
 
