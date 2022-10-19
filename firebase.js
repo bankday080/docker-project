49dca83f404d0a1faf3d4670513445a2a72412b4
bankday080 <bankday02@gmail.com>
2022-10-19 21:33:33 +0700

    no message

--- firebase.js
+++ firebase.js
@@ -5,7 +5,7 @@ const { query } = require("express");
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
-  databaseURL: "https://egg-project-f7924-default-rtdb.asia-southeast1.firebasedatabase.app"
+  // databaseURL: "https://egg-project-f7924-default-rtdb.asia-southeast1.firebasedatabase.app"
 });
 
 const db = admin.firestore();
